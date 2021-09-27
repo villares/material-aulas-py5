20ba577009fdf1dc698a2b5bcac2d65aa34ff93b
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2021-09-27 13:21:29 -0300

    tests

---
+++ build_book.sh
@@ -0,0 +1,3 @@
+jb clean .
+jb build .
+ghp-import -n -p -f _build/html
---
+++ push_book.sh
@@ -0,0 +1,8 @@
+git add -A
+git commit -m "saving modifications `date --rfc-3339=seconds`"
+git push
+git checkout gh-pages
+git add -A
+git commit -m "saving modifications `date --rfc-3339=seconds`"
+git push
+git checkout main
